bbe4decc701eec4fee2a896bffd0a4762fd43ace
Mateusz <mateusz.budek@cyfroweszkoly.pl>
2024-12-18 20:04:31 +0100

    Make special methods for interfaces in PlayerType.cs

--- Board-game/Board-game/PlayerType.cs
+++ Board-game/Board-game/PlayerType.cs
@@ -5,7 +5,9 @@ public interface Warrior
     // Warrior (Wojownik) - specjalizuje się w zdobywaniu dużej ilości punktów przez walkę wręcz
     public void Attack(Player attacking, Player attacked)
     {
+        var attackPower = attacking.Strength;
         Console.WriteLine($"{attacking.Name} atakuje {attacked.Name}!");
+        attacked.Health = (int)(attacked.Health - attackPower + Math.Floor(attackPower * 0.2));
     }
 }
 
@@ -14,7 +16,9 @@ public interface Archer
     // Archer (Łucznik) - specjalizuje się w zdobywaniu dużej ilości punktów przez walkę na dystans
     public void Attack(Player attacking, Player attacked)
     {
+        var attackPower = attacking.Strength;
         Console.WriteLine($"{attacking.Name} atakuje {attacked.Name}!");
+        attacked.Health = (int)(attacked.Health - attackPower + Math.Floor(attackPower * 0.2));
     }
 }
 
@@ -22,22 +26,40 @@ public interface Mage
 {
     // Mage (Mag) - specjalizuje się w rzucaniu zaklęć, aby wpływać na grę
     int Mana { get; set; }
-    public void Attack(Player attacking, Player attacked)
+    public void Magic(Player mage, Random random)
     {
-        Console.WriteLine($"{attacking.Name} atakuje {attacked.Name}!");
+        Console.WriteLine($"{mage.Name} czaruje!");
+        var rand = random.Next(1, 4);
+        var points = random.Next(1, 11);
+        var move = random.Next(1, 5);
+        switch (rand)
+        {
+            case 1:
+                Console.WriteLine($"{mage.Name} wyczarowuje {rand} punktów!");
+                mage.Score += points;
+                break;
+            case 2:
+                Console.WriteLine($"{mage.Name} przenosi się o {move} pola do przodu!");
+                mage.Position += move;
+                break;
+            case 3:
+                Console.WriteLine($"{mage.Name} przenosi się o {move} pola do tyłu!");
+                mage.Position += move;
+                break;
+            default:
+                Console.WriteLine("Łot? Nieznana liczba?");
+                break;
+        }
     }
 }
 public interface Healer
 {
     // Healer (Lekarz) - specjalizuje się w leczeniu innych graczy
     int Mana { get; set; }
-    public void Attack(Player attacking, Player attacked)
-    {
-        Console.WriteLine($"{attacking.Name} atakuje {attacked.Name}!");
-    }
 
-    public void Heal(Player healer, int healPoints)
+    public void Heal(Player healer, Random random)
     {
+        var healPoints = random.Next(1, 16);
         Console.WriteLine($"{healer.Name} ulecza siebie o {healPoints} punktów!");
         healer.Health += healPoints;
     }
