d1650d44b496dde4a21c2da988c26cfae472252e
Szymon <gutek.gutomski@gmail.com>
2024-12-22 15:12:48 +0100

    Pierwszy commit

---
+++ ConcertTicketBookingSystem/Concert.cs
@@ -0,0 +1,27 @@
+﻿public class Concert{
+    public string Name{get;set;}
+    public DateTime Date{get;set;}
+    public string Location{get;set;}
+    public int AvailableSeats{get;set;}
+
+    public Concert(string name, DateTime date, string location, int availableSeats)
+    {
+        Name = name;
+        Date = date;
+        Location = location;
+        AvailableSeats = availableSeats;
+    }
+
+    public void ReserveSeat()
+    {
+        if (AvailableSeats > 0)
+        {
+            AvailableSeats--;
+        }
+        else
+        {
+            Console.WriteLine("Brak dostępnych miejsc");
+        }
+    }
+
+}
