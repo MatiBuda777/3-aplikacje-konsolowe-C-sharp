4dc02be7a6b011890262f7c1727c883f95059fd3
Szymon <gutek.gutomski@gmail.com>
2024-12-22 21:55:09 +0100

    Online concert

--- ConcertTicketBookingSystem/Typykoncertow.cs
+++ ConcertTicketBookingSystem/Typykoncertow.cs
@@ -13,3 +13,13 @@ public class VIPConcert : Concert, IConcertType
         return"VIP Concert";
     }
 }
+
+public class OnlineConcert : Concert, IConcertType
+{
+    public OnlineConcert(string name, DateTime date, string location, int availableSeats):base(name, date, location, availableSeats){}
+
+    public string GetConcertType()
+    {
+        return"Online Concert";
+    }
+}
