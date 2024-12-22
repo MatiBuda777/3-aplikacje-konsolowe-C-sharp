b8d27da1e5aaf5a97981282e882299636f46ec73
Szymon <gutek.gutomski@gmail.com>
2024-12-22 20:52:45 +0100

    Klasa, metody

--- ConcertTicketBookingSystem/BookingSystem.cs
+++ ConcertTicketBookingSystem/BookingSystem.cs
@@ -2,5 +2,13 @@ namespace DefaultNamespace;
 
 public class BookingSystem
 {
-    
+    private List<Concert> concerts=new List<Concert>()
+    private List<Ticket> tickets = new List<Ticket>()
+
+
+    public void AddConcert(Concert concert)
+    {
+        concerts.Add(concert);
+    }
+    public void DisplayConcerts()
 }
