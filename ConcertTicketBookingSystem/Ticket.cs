8ceb36aa7f8a787070110b5ee674473464648478
Szymon <gutek.gutomski@gmail.com>
2024-12-22 20:32:31 +0100

    Właściwosci, Konstrukor

--- ConcertTicketBookingSystem/Ticket.cs
+++ ConcertTicketBookingSystem/Ticket.cs
@@ -2,5 +2,16 @@ namespace DefaultNamespace;
 
 public class Ticket
 {
+    public Concert Concert { get; set; }
+    public int SeatNumber { get; set; }
+    public decimal Price { get; set; }
+    
+    //konstruktor
+    public Ticket(this.concert, int SeatNumber, decimal Price)
+    {
+        Concert = Concert;
+        Price = Price;
+        SeatNumber = SeatNumber;
+    }
     
 }
